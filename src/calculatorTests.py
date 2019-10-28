cf74614f4f0e86d5f3ac350ccb9bc465082f7747
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-28 15:52:23 -0400

    feature: added contents to calculator and calculatorTests

---
+++ src/calculatorTests.py
@@ -0,0 +1,10 @@
+import unittest
+from calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+    def test_instantiate_calculator(self):
+        calculator=Calculator()
+        self.assertIsInstance(calculator,Calculator)
+
+if __name__ == '__main__':
+    unittest.main()
