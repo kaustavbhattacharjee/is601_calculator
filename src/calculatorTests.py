e59c6eefb81dc00e2ea3d77e19e307183f649022
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-28 20:12:33 -0400

    feature: added subtraction test

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -1,14 +1,72 @@
-import unittest
+import unittest,csv
 from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
+
+
     def test_instantiate_calculator(self):
         calculator=Calculator()
         self.assertIsInstance(calculator,Calculator)
 
-    def test_results_property_calculator(self):
-        calculator=Calculator()
-        #self.assertEqual(calculator.result,3)
+    def test_add(self):
+        calculator = Calculator()
+        test_add_file_path = "./src/addition.csv"
+        csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
+        next(csv_reader)
+        test_add_row_list=list()
+        for row in csv_reader:
+            test_add_row_list.append(row)
+        for row in test_add_row_list:
+            a=int(row[0])
+            b=int(row[1])
+            c=int(row[2])
+            exp_result=calculator.add(a,b)
+            self.assertEqual(exp_result,c)
+
+    def test_subtract(self):
+        calculator = Calculator()
+        test_add_file_path = "./src/subtraction.csv"
+        csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
+        next(csv_reader)
+        test_add_row_list=list()
+        for row in csv_reader:
+            test_add_row_list.append(row)
+        for row in test_add_row_list:
+            a=int(row[0])
+            b=int(row[1])
+            c=int(row[2])
+            exp_result=calculator.subtract(a,b)
+            self.assertEqual(exp_result,c)
+
+    def test_multiply(self):
+        calculator = Calculator()
+        test_add_file_path = "./src/multiplication.csv"
+        csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
+        next(csv_reader)
+        test_add_row_list=list()
+        for row in csv_reader:
+            test_add_row_list.append(row)
+        for row in test_add_row_list:
+            a=int(row[0])
+            b=int(row[1])
+            c=int(row[2])
+            exp_result=calculator.multiply(a,b)
+            self.assertEqual(exp_result,c)
+
+    def test_divide(self):
+        calculator = Calculator()
+        test_add_file_path = "./src/division.csv"
+        csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
+        next(csv_reader)
+        test_add_row_list=list()
+        for row in csv_reader:
+            test_add_row_list.append(row)
+        for row in test_add_row_list:
+            a=int(row[0])
+            b=int(row[1])
+            c=int(row[2])
+            exp_result=calculator.divide(a,b)
+            self.assertEqual(exp_result,c)
 
 
 if __name__ == '__main__':
