c1e39cd1b9542264046a1239917d69ca48678fef
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-28 20:14:36 -0400

    feature: added multiplication test

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -53,20 +53,7 @@ class MyTestCase(unittest.TestCase):
             exp_result=calculator.multiply(a,b)
             self.assertEqual(exp_result,c)
 
-    def test_divide(self):
-        calculator = Calculator()
-        test_add_file_path = "./src/division.csv"
-        csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
-        next(csv_reader)
-        test_add_row_list=list()
-        for row in csv_reader:
-            test_add_row_list.append(row)
-        for row in test_add_row_list:
-            a=int(row[0])
-            b=int(row[1])
-            c=int(row[2])
-            exp_result=calculator.divide(a,b)
-            self.assertEqual(exp_result,c)
+
 
 
 if __name__ == '__main__':
