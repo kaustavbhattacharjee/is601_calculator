fd2fba4861a5c636d2746523219a7eb264b5ad76
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-29 20:44:08 -0400

    fix: update for travis build

--- CsvReader/my_csv_reader.py
+++ CsvReader/my_csv_reader.py
@@ -1,8 +1,9 @@
-import csv
+import csv,os
 from pathlib import Path
 def read_csv(path):
     data_folder=Path("/Tests/Data/")
-    test_add_file_path = data_folder / path
+    test_add_file_path1 = data_folder / path
+    test_add_file_path=test_add_file_path1.absolute()
     csv_reader = csv.reader(open(test_add_file_path, 'r'), delimiter=',')
     next(csv_reader)
     test_row_list = list()
